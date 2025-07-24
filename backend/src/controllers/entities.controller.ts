e99e5355f73100b7b14d10ab05553c7d86953b6a
zanellig <gonzalozanelli1@gmail.com>
2025-07-23 23:15:39 -0300

    feat(entities): add DTO validation and user association
    - Implement DTO validation for entity creation and retrieval
    - Associate entities with users during creation
    - Add conflict check for existing entities
    - Improve error handling with proper status codes

--- backend/src/controllers/entities.controller.ts
+++ backend/src/controllers/entities.controller.ts
@@ -1,29 +1,59 @@
-import type { Context } from "hono";
 import db from "../services/db";
 import { entities } from "../models/entities.model";
 import { eq } from "drizzle-orm";
+import {
+  createEntityDto,
+  createEntityResponseDto,
+  getEntitiesDto,
+  getEntityDto,
+} from "../dtos/entities.dto";
+
+import type { Context } from "hono";
+import { users } from "../models/users.model";
 
 export async function getEntities(c: Context) {
   const entitiesRes = await db.select().from(entities);
-  return c.json({ data: entitiesRes }, entitiesRes.length ? 200 : 404);
+  const entitiesDto = getEntitiesDto.safeParse(entitiesRes);
+  return c.json(entitiesDto, entitiesDto.success ? 200 : 404);
 }
 
 export async function getEntity(c: Context) {
   const entityId = c.req.param("id");
-
   if (entityId) {
     const entityRes = await db
       .select()
       .from(entities)
       .where(eq(entities.id, entityId));
-    return c.json({ data: entityRes }, entityRes.length ? 200 : 404);
+    const entityDto = getEntityDto.safeParse(entityRes[0]);
+    return c.json(entityDto, entityDto.success ? 200 : 404);
   }
-
-  return c.json({ data: null }, 404);
+  return c.json({ data: null }, 400);
 }
 
 export async function createEntity(c: Context) {
   const entity = await c.req.json();
-  const entityRes = await db.insert(entities).values(entity);
-  return c.json({ data: entityRes }, 201);
+  const entityInsertor = createEntityDto.safeParse(entity);
+  if (!entityInsertor.success) {
+    console.log(entityInsertor);
+    return c.json({ data: null }, 400);
+  }
+  const [user] = await db.select().from(users);
+  const existingEntity = await db
+    .select({
+      id: entities.id,
+    })
+    .from(entities)
+    .where(eq(entities.name, entityInsertor.data.name));
+  if (existingEntity.length) {
+    return c.json(
+      { error: "Entity already exists", data: existingEntity[0] },
+      409
+    );
+  }
+  const [res] = await db
+    .insert(entities)
+    .values({ userId: user.id, ...entityInsertor.data })
+    .$returningId();
+
+  return c.json(createEntityResponseDto.safeParse(res), 201);
 }
