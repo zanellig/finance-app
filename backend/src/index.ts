c50e90205a19b2046bd31c97eb3f1419c6d1e3bf
zanellig <gonzalozanelli1@gmail.com>
2025-07-31 15:33:06 -0300

    feat: add compression middleware and document controller refactoring notes
    Add compress middleware from @hono/bun-compress to improve response sizes. Include documentation comments about recommended Hono controller patterns and note the need for future refactoring to support dynamic route parameters.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { etag } from "hono/etag";
 import { logger } from "hono/logger";
 import { cors } from "hono/cors";
+import { compress } from "@hono/bun-compress";
 
 import * as entities from "./controllers/entities.controller";
 import * as users from "./controllers/users.controller";
@@ -10,10 +11,32 @@ import { createEntityDto } from "./dtos/entities.dto";
 
 import { validateBody } from "./utils/validator";
 
+/**
+ * This way of writing Rails-like controllers is not recommended in the Hono docs.
+ * Instead, we sould do the following:
+ * ```ts
+ * // entities.route.ts
+ * import { Hono } from "hono";
+ * const entities = new Hono();
+ *
+ * entities.get("/", (c)=>{
+ *   return c.json({foo: "bar"})
+ * });
+ *
+ * export default entities;
+ * ```
+ *
+ * **This is because if we want to use a dynamic route parameter, it can't be inferred.**
+ *
+ * @see https://hono.dev/docs/guides/best-practices
+ *
+ * **TODO**: Refactor controllers
+ */
 const app = new Hono();
 
 app.use(etag(), logger());
 app.use("/api/*", cors());
+app.use(compress());
 
 app
   .get("/api/entities", entities.getEntities)
