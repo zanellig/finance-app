cb74c41d136736606d954f334c8f580971095932
zanellig <gonzalozanelli1@gmail.com>
2025-08-03 23:12:25 -0300

    feat(models): add income model with required fields and relations
    Add new income model with fields for amount, frequency, and timestamps.
    Includes relation to users model and default values for common fields.

---
+++ backend/src/models/income.model.ts
@@ -0,0 +1,36 @@
+import {
+  boolean,
+  decimal,
+  mysqlEnum,
+  mysqlTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/mysql-core";
+
+import { v4 } from "uuid";
+import { users } from "./users.model";
+import { defaultTimestamps, noActionCascade } from "./constants";
+
+export const income = mysqlTable("income", {
+  id: varchar({ length: 36 }).primaryKey().unique().$defaultFn(v4),
+  userId: varchar("user_id", { length: 36 }).references(
+    () => users.id,
+    noActionCascade
+  ),
+  name: varchar({ length: 255 }),
+  amount: decimal({
+    precision: 2,
+  })
+    .notNull()
+    .default("0.00"),
+  frequency: mysqlEnum([
+    "monthly",
+    "biweekly",
+    "quarterly",
+    "annually",
+  ]).notNull(),
+  isHourly: boolean("is_hourly").default(false),
+  startDate: timestamp("start_date").notNull(),
+  endDate: timestamp("end_date"),
+  ...defaultTimestamps,
+});
