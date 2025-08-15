24694ed6927ab186a8707837a29ac94c677af79f
zanellig <gonzalozanelli1@gmail.com>
2025-08-15 03:13:35 -0300

    refactor(config): update database credentials configuration to use MYSQL_URL to avoid weird AWS Drizzle error

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from "drizzle-kit";
 import { env } from "./src/config/env";
 
 export default defineConfig({
+  out: "./drizzle",
   dialect: "mysql",
   dbCredentials: {
-    host: env.DB_HOST,
-    port: Number(env.DB_PORT) || 3306,
-    user: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_NAME,
+    url: env.MYSQL_URL,
+    // host: env.DB_HOST,
+    // port: Number(env.DB_PORT) || 3306,
+    // user: env.DB_USER,
+    // password: env.DB_PASSWORD,
+    // database: env.DB_NAME,
   },
   schema: "./src/models",
   casing: "snake_case",
